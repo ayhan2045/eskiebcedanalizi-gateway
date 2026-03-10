d3a25297b03026f679b9a00b63da3f2bed4f052d
ayhan <ayanbeyan51@gmail.com>
2026-03-10 05:07:14 +0300

    Update start.sh to use GATEWAY variables and remove BASIC_AUTH

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-: "${BASIC_AUTH_USER:?BASIC_AUTH_USER is required}"
-: "${BASIC_AUTH_PASS:?BASIC_AUTH_PASS is required}"
+: "${GATEWAY_PASSWORD:?GATEWAY_PASSWORD is required}"
+: "${GATEWAY_SECRET:?GATEWAY_SECRET is required}"
 
 export PORT="${PORT:-8080}"
+export AUTH_PORT="${AUTH_PORT:-9000}"
 
-htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASS"
+envsubst '${PORT} ${AUTH_PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+python3 -u /auth_server.py &
 
 nginx -t
 exec nginx -g 'daemon off;'
